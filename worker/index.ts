ee83c17c1cb2664422bd93e5839f32d0e3d326b8
Craig Dennis <craigsdennis@gmail.com>
2025-07-31 12:50:55 -0700

    Unused dep

--- worker/index.ts
+++ worker/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { streamText } from "hono/streaming";
 import OpenAI from "openai";
-import type { Tool } from "openai/resources/responses/responses.mjs";
 
 const app = new Hono<{ Bindings: Env }>();
 
